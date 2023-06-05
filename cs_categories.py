cf082b0cdd593df06747c6ff58c348789ac75941
Kjell Hedstrom <kjell@ganaz.com>
2023-06-05 16:15:09 -0600

    fixed outout

--- cs_categories.py
+++ cs_categories.py
@@ -38,6 +38,11 @@ def datetime_serializer(obj):
         )
 
 
+# Function to calculate resolution_date based on weeks_back input
+def get_resolution_date(weeks_back):
+    return (date.today() - timedelta(weeks=weeks_back)).strftime("%Y-%m-%d")
+
+
 class DateTimeEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime):
@@ -292,12 +297,19 @@ def update_aggregated_results(xops_time_records, ticket_data, label):
             adjusted_duration_s = interval["adjusted_duration_s"]
             total_adjusted_in_review_seconds += adjusted_duration_s
 
+    average_in_progress_s = (
+        total_adjusted_in_progress_seconds / total_tickets if total_tickets > 0 else 0
+    )
+    average_in_review_s = (
+        total_adjusted_in_review_seconds / total_tickets if total_tickets > 0 else 0
+    )
+
     xops_time_records[label] = {
         "total_tickets": len(ticket_data),
-        "total_in_progress_s": total_adjusted_in_progress_seconds,
-        "total_in_review_s": total_adjusted_in_review_seconds,
-        "average_in_progress_s": (total_adjusted_in_progress_seconds / total_tickets),
-        "average_in_review_s": (total_adjusted_in_review_seconds / total_tickets),
+        "total_in_progress": total_adjusted_in_progress_seconds,
+        "total_in_review": total_adjusted_in_review_seconds,
+        "average_in_progress": average_in_progress_s,
+        "average_in_review": average_in_review_s,
         "total_tickets": total_tickets,
     }
 
@@ -328,18 +340,21 @@ def print_detailed_ticket_data(ticket_data):
 
 
 # Fetch all issues with specific label
-def fetch_issues_by_label(jira, label):
+def fetch_issues_by_label(jira, label, resolution_date):
     issues = []
     start_index = 0
     max_results = 100
 
     query = (
-        "project = GAN  AND status in (Closed) "
-        'and labels="{label}" '
-        'and resolution NOT IN ("Duplicate", "Won\'t Do", "Declined") '
-        "and resolutiondate > '2022-01-01' "
-        "order by resolved desc"
+        f"project = GAN  AND status in (Closed) "
+        f'and labels="{label}" '
+        f'and resolution NOT IN ("Duplicate", "Won\'t Do", "Declined") '
+        f"and resolutiondate > '{resolution_date}' "
+        f"order by resolved desc"
     )
+
+    print(f" Executing query:\n\t [{query}]")
+
     while True:
         chunk = jira.search_issues(
             jql_str=query.format(label=label),
@@ -357,7 +372,7 @@ def fetch_issues_by_label(jira, label):
     return issues
 
 
-def retrieve_jira_query_issues(args, jira, label):
+def retrieve_jira_query_issues(args, jira, label, resolution_date):
     issues = {}
     jira_file = f"{label}_data.json"
     if args.load:
@@ -370,13 +385,62 @@ def retrieve_jira_query_issues(args, jira, label):
         issues = load_jira_data_from_file(jira_file, jira)
         print(f"Load jira {len(issues)} tickets from {jira_file}")
     else:
-        issues = fetch_issues_by_label(jira, label)
+        issues = fetch_issues_by_label(jira, label, resolution_date)
         print(f'Fetched {len(issues)} issues with label "{label}"...')
 
     if args.save:
         print(f"Saving JIRA {len(issues)} issues to {jira_file}")
         save_jira_data_to_file(issues, jira_file)
     return issues
+
+
+# ... (Imports and other functions here)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Query with custom timeframe.")
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Display detailed ticket data"
+    )
+    parser.add_argument(
+        "-l",
+        "--load",
+        action="store_true",
+        help="Load from previously stored json dump of JIRA data",
+    )
+    parser.add_argument(
+        "-s",
+        "--save",
+        action="store_true",
+        help="Save a json dump from the JIRA data you will now retrieve, you can later use the -l --load function and avoid JIRA query overhead",
+    )
+
+    parser.add_argument(
+        "--resolution-date",
+        type=str,
+        metavar="YYYY-MM-DD",
+        help="Enter a specific resolution date in YYYY-MM-DD format",
+    )
+
+    parser.add_argument(
+        "--weeks-back",
+        type=int,
+        help="Enter how many weeks back from today to set the resolution date",
+    )
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Get the resolution date parameter
+    if args.resolution_date:
+        resolution_date = args.resolution_date
+    elif args.weeks_back:
+        resolution_date = get_resolution_date(args.weeks_back)
+    else:
+        parser.print_help()
+        exit(1)
+
+    return args, resolution_date
 
 
 # # https://ganazhq.slack.com/archives/C03BR47GDQW/p1684342201816599
@@ -393,25 +457,7 @@ def retrieve_jira_query_issues(args, jira, label):
 # # xops_carholder_assorted
 # # https://ganaz.atlassian.net/browse/GAN-5750
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-v", "--verbose", action="store_true", help="Display detailed ticket data"
-    )
-    parser.add_argument(
-        "-l",
-        "--load",
-        action="store_true",
-        help="Load from previously stored json dump of JIRA data",
-    )
-    parser.add_argument(
-        "-s",
-        "--save",
-        action="store_true",
-        help="Save a json dump from the JIRA data you will now retrieve, you can later use the -l --load function and avoid JIRA query overhead",
-    )
-    args = parser.parse_args()
-    if args.verbose:
-        print("Verbose mode is on.")
+    args, resolution_date = parse_arguments()
 
     xops_labels = [
         "xops_packet_update",
@@ -425,8 +471,19 @@ def main():
         "xops_company_employee_id_counter",
         "xops_raffle",
         "xops_reports",
-        "xops_carholder_assorted"
+        "xops_carholder_assorted",
     ]
+
+    default_metrics = {
+        "tickets_completed": 0,
+        "total_in_progress": 0,
+        "total_in_review": 0,
+        "average_in_progress": 0,
+        "average_in_review": 0,
+    }
+
+    # Generate data list from xops_labels and default_metrics
+    data = {label: dict(default_metrics) for label in xops_labels}
 
     # Perform JQL query and handle pagination if needed
     user = os.environ.get("USER_EMAIL")
@@ -441,20 +498,28 @@ def main():
     # Loop through each xops_label, fetch issues and process results
     xops_time_records = {}
     for label in xops_labels:
-        issues = retrieve_jira_query_issues(args, jira, label)
+        issues = retrieve_jira_query_issues(args, jira, label, resolution_date)
         print(f'Processing {len(issues)} issues with label "{label}"...')
         ticket_data = process_issues(jira, issues, custom_fields_map)
 
         if args.verbose:
             print_detailed_ticket_data(ticket_data)
 
-        update_aggregated_results(xops_time_records, ticket_data, label)
+        time_records = update_aggregated_results(xops_time_records, ticket_data, label)
+        data[label] = time_records[label]
         print(f"total tickets: {len(ticket_data)}")
 
-    # # Now you can calculate averages, total costs, and other required aggregations using final_results.
-    # # Display aggregated results for each xops_label
-    for label in xops_labels:
-        print_time_records(label, xops_time_records[label])
+    # Convert data dictionary into list of dicts
+    data_list = [{**{"label": label}, **values} for label, values in data.items()]
+    # Sort and print the records
+    sorted_data = sorted(data_list, key=lambda x: x["total_tickets"], reverse=True)
+
+    for item in sorted_data:
+        print(f"Label: {item['label']} {item['total_tickets']} tickets completed")
+        print(f"\tTotal In Progress   (m): {item['total_in_progress']/60:>7.2f}")
+        print(f"\tTotal In Review     (m): {item['total_in_review']/60:>7.2f}")
+        print(f"\tAverage In Progress (m): {item['average_in_progress']/60:>7.2f}")
+        print(f"\tAverage In Review   (m): {item['average_in_review']/60:>7.2f}\n")
 
 
 if __name__ == "__main__":
