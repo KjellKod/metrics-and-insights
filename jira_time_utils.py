080df5c2a0069a4fec81d52b45cf58d0cbbfb4d0
Kjell Hedstrom <kjell@ganaz.com>
2023-08-07 16:13:57 -0600

    pylint fixed time handling

--- jira_time_utils.py
+++ jira_time_utils.py
@@ -2,47 +2,53 @@
 date, timezone, workdays conversion and handling
 """
 
+import json
 from datetime import datetime, date, timedelta
 from dateutil.relativedelta import relativedelta
-
-import json
 import numpy as np
 
 
 def datetime_serializer(obj):
+    """needed for handling datatime objects"""
     if isinstance(obj, (datetime, date)):
         return obj.isoformat()
-    elif isinstance(obj, np.int64):
+
+    if isinstance(obj, np.int64):
         return int(obj)
-    else:
-        raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
+
+    raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
 
 
-# Function to calculate resolution_date based on weeks_back input
 def get_resolution_date(weeks_back):
+    """Function to calculate resolution_date based on weeks_back input"""
     return (date.today() - timedelta(weeks=weeks_back)).strftime("%Y-%m-%d")
 
 
 class DateTimeEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, datetime):
-            return obj.isoformat()
-        return super(DateTimeEncoder, self).default(obj)
+    """helper"""
+
+    def default(self, o):
+        if isinstance(o, datetime):
+            return o.isoformat()
+        return super().default(o)  # same as: super(DateTimeEncoder, self).default(o)
 
 
 def parse_date(date_str):
+    "date parse helper"
     if date_str is not None:
         return datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f%z")
     return None
 
 
 def seconds_to_hms(seconds):
+    """seconds to hours minutes seconds"""
     hours, remainder = divmod(seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
     return hours, minutes, seconds
 
 
 def business_time_spent_in_seconds(start, end):
+    """extract only the time spent during business hours from a jira time range"""
     weekdays = [0, 1, 2, 3, 4]  # Monday to Friday
     total_business_seconds = 0
     seconds_in_workday = 8 * 60 * 60  # 8 hours * 60 minutes * 60 seconds
@@ -69,6 +75,7 @@ def business_time_spent_in_seconds(start, end):
 
 
 def get_week_intervals(minimal_date, maximal_date, interval):
+    """extract week intervals"""
     minimal_date = datetime.strptime(minimal_date, "%Y-%m-%d")
     maximal_date = datetime.strptime(maximal_date, "%Y-%m-%d")
 
