2d9cfdf7cebb822eb5f4c56465be1aeed446cea3
KjellKod <kjell.hedstrom@gmail.com>
2024-09-06 17:24:35 -0600

    engineering_excellence.py

--- jira/engineering_excellence.py
+++ jira/engineering_excellence.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import os
 from jira import JIRA
 from collections import defaultdict
+import json
 
 # Jira API endpoint
 username = os.environ.get('USER_EMAIL')
@@ -35,39 +36,70 @@ def get_jira_instance():
 
 
 
+def get_resolution_date(ticket):
+    for history in reversed(ticket.changelog.histories):
+        for item in history.items:
+            if item.field == "status" and item.toString == "Released":
+                return datetime.strptime(history.created, "%Y-%m-%dT%H:%M:%S.%f%z")
+    return None
 
 def print_engineering_excellence(): 
     jira = get_jira_instance()
     current_year = datetime.now().year
-    start_date = f"{current_year}-01-01"
+    start_date = f"{current_year}-09-01"
     end_date = f"{current_year}-12-31"
 
-    # original jql_query = f"(project = ENG OR project = ONF) AND status = Done AND updatedDate >= {start_date} AND updatedDate <= {end_date} ORDER BY updatedDate ASC"
-    # TODO the query below for released tickets work the one above does not really work.
-    jql_query = f"project in (ONF, ENG, MOB) AND status in (Released) and (updatedDate >= {start_date} and updatedDate <= {end_date}) AND issueType in (Task, Bug, Story, Spike) ORDER BY updated ASC"
+    # Modified JQL query to filter tickets that changed to "Released" status within the given timeframe
+    jql_query = f"project in (ONF, ENG, MOB) AND status changed to Released during ({start_date}, {end_date}) AND issueType in (Task, Bug, Story, Spike) ORDER BY updated ASC"
 
+    print(jql_query) 
 
-    issues = jira.search_issues(jql_query, maxResults=None)
-    month_data = defaultdict(lambda: {"engineering_excellence": 0, "other": 0})
+    released_tickets = jira.search_issues(jql_query, maxResults=1000, expand='changelog')
+    team_data = defaultdict(lambda: defaultdict(lambda: {"engineering_excellence": 0, "other": 0}))
+    print(f"Total number of tickets retrieved: {len(released_tickets)}")
 
-    for issue in issues:
-        resolution_date = datetime.strptime(issue.fields.updated, "%Y-%m-%dT%H:%M:%S.%f%z")
+    for ticket in released_tickets:
+        resolution_date = get_resolution_date(ticket)
+        if not resolution_date:
+            print(f"Ticket {ticket.key} has no resolution date")
+            continue
         month_key = resolution_date.strftime("%Y-%m")
+        work_type = ticket.fields.customfield_10079
+        team_field = ticket.fields.customfield_10075
 
-        engineering_excellence = issue.fields.customfield_10079
-        if engineering_excellence in ["Debt Reduction", "Critical"]:
-            month_data[month_key]["engineering_excellence"] += 1
+        if ticket.fields.project.key == "MOB":
+            team = "mobile"
+        elif team_field is not None:
+            team = team_field.value.strip()
         else:
-            month_data[month_key]["other"] += 1
+            team = "unknown"
 
-    for month, data in sorted(month_data.items()):
-        total_tickets = data["engineering_excellence"] + data["other"]
-        percentage = (data["engineering_excellence"] / total_tickets) * 100 if total_tickets > 0 else 0
-        print(f"Month: {month}")
-        print(f"Engineering Excellence Tickets: {data['engineering_excellence']}")
-        print(f"Other Tickets: {data['other']}")
-        print(f"Percentage of Engineering Excellence Work: {percentage:.2f}%")
-        print("---")
+        if work_type is not None:
+            work_type_value = work_type.value.strip()
+        else:
+            work_type_value = "Other"
+
+        if work_type_value in ["Debt Reduction", "Critical"]:
+            team_data[team][month_key]["engineering_excellence"] += 1
+            #print(f"{ticket.key} - {work_type_value} - {team}")
+        else:
+            team_data[team][month_key]["other"] += 1
+            #print(f"{month_key} {ticket.key} - {work_type_value} - {team}")
+
+    for team, months in sorted(team_data.items()):
+        print(f"Team {team.capitalize()}")
+        for month, data in sorted(months.items()):
+            total_tickets = data["engineering_excellence"] + data["other"]
+            if total_tickets > 0:
+                product_focus_percent = (data["other"] / total_tickets) * 100
+                engineering_excellence_percent = (data["engineering_excellence"] / total_tickets) * 100
+            else:
+                product_focus_percent = 0
+                engineering_excellence_percent = 0
+
+            print(f"  {month} Total tickets: {total_tickets}, product focus: {data['other']} [{product_focus_percent:.2f}%], engineering excellence: {data['engineering_excellence']} [{engineering_excellence_percent:.2f}%]")
+
+
 
 print_engineering_excellence()
     
