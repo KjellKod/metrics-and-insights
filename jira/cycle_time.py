633de0e9c7dc52c240d95e840a7c8e851c7c0e7d
KjellKod <kjell.hedstrom@gmail.com>
2024-09-12 17:41:58 -0600

    refactoring for business time and choosing when to show verbose printouts

--- jira/cycle_time.py
+++ jira/cycle_time.py
@@ -1,10 +1,20 @@
 import os
-from jira import JIRA
-from jira.resources import Issue
+import statistics
 from collections import defaultdict
 from datetime import datetime, timedelta
-import statistics 
+import argparse
+
 import pytz
+from jira import JIRA
+from jira.resources import Issue
+
+
+# Define the argument parser
+parser = argparse.ArgumentParser(description="Process some tickets.")
+parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose output')
+
+# Parse the arguments
+args = parser.parse_args()
 
 # Jira API endpoint
 username = os.environ.get('USER_EMAIL')
@@ -16,9 +26,14 @@ for var in required_env_vars:
     if os.environ.get(var) is None:
         raise ValueError(f"Environment variable {var} is not set.")
 
-
 hours_to_days = 8
 seconds_to_hours = 3600
+
+def verbose_print(message):
+    if args.verbose:
+        print(message)
+
+
 
 def get_jira_instance():
     """
@@ -40,9 +55,8 @@ def get_jira_instance():
 def get_tickets_from_jira(start_date, end_date):
     # Get the Jira instance
     jira = get_jira_instance()
-    #jql_query = f"project in ({', '.join(projects)}) AND status in (Released) and (updatedDate >= {start_date} and updatedDate <= {end_date}) AND issueType in (Task, Bug, Story, Spike) ORDER BY updated ASC"
-    jql_query = f"key in ('INT-161', 'INT-1917', 'INT-1922') AND status in (Released) and (updatedDate >= {start_date} and updatedDate <= {end_date}) AND issueType in (Task, Bug, Story, Spike) ORDER BY updated ASC"
-
+    jql_query = f"project in ({', '.join(projects)}) AND status in (Released) and (updatedDate >= {start_date} and updatedDate <= {end_date}) AND issueType in (Task, Bug, Story, Spike) ORDER BY updated ASC"
+    
     max_results = 100
     start_at = 0
     total_tickets = []
@@ -64,6 +78,47 @@ def validate_issue(issue):
         print(f"Unexpected type: {type(issue)}  -- ignoring")
         return False
     return True
+
+
+def get_team(ticket):
+    team_field = ticket.fields.customfield_10075
+    if team_field:
+        return team_field.value.strip().lower().capitalize()
+    project_key = ticket.fields.project.key.upper()
+    default_team = os.getenv(f'TEAM_{project_key}')
+    
+    if default_team:
+        return default_team.strip().lower().capitalize()
+    else:
+        return project_key.strip().lower().capitalize()
+
+
+def business_time_spent_in_seconds(start, end):
+    """extract only the time spent during business hours from a jira time range -- only count 8h"""
+    weekdays = [0, 1, 2, 3, 4]  # Monday to Friday
+    total_business_seconds = 0
+    seconds_in_workday = 8 * 60 * 60  # 8 hours * 60 minutes * 60 seconds
+
+    current = start
+    while current <= end:
+        if current.weekday() in weekdays:
+            day_end = current.replace(hour=23, minute=59)
+            remaining_time_today = day_end - current
+
+            if current.date() != end.date():
+                total_business_seconds += min(remaining_time_today.total_seconds(), seconds_in_workday)
+                current += timedelta(days=1)
+                current = current.replace(hour=0, minute=0)
+            else:
+                remaining_time_on_last_day = end - current
+                total_business_seconds += min(remaining_time_on_last_day.total_seconds(), seconds_in_workday)
+                break
+        else:
+            current += timedelta(days=1)
+            current = current.replace(hour=0, minute=0)
+
+    return total_business_seconds
+
 
 def localize_start_date(start_date_str):
     pst = pytz.timezone('America/Los_Angeles')
@@ -114,7 +169,7 @@ def calculate_business_time(code_review_timestamp, released_timestamp):
     return business_seconds, business_days
 
 def log_cycle_time(issue_key, log_string, business_seconds, business_days, code_review_timestamp, released_timestamp):
-    log_string += f"Cycle time in business hours: {business_seconds / seconds_to_hours:.2f} --> days: {business_seconds / (3600 * 8):.2f}\n"
+    log_string += f"Cycle time in business hours: {business_seconds / seconds_to_hours:.2f} --> days: {business_seconds / (seconds_to_hours * 8):.2f}\n"
     log_string += f"Review started at: {code_review_timestamp}, released at: {released_timestamp}, Cycle time: {business_days} days\n"
     log_string += f"Cycle time in hours: {business_seconds / 3600:.2f} --> days: {business_seconds / (3600 * 8):.2f}\n"
     return log_string
@@ -131,115 +186,67 @@ def calculate_cycle_time_seconds(start_date_str, issue):
         business_seconds, business_days = calculate_business_time(code_review_timestamp, released_timestamp)
         log_string = log_cycle_time(issue.key, log_string, business_seconds, business_days, code_review_timestamp, released_timestamp)
         month_key = released_timestamp.strftime("%Y-%m")
-        print(f"Processing {issue.key}\n{log_string}")
+        verbose_print(f"Processing {issue.key}\n{log_string}")
         return business_seconds, month_key
     return None, None
 
 def calculate_monthly_cycle_time(start_date, end_date):
     tickets = get_tickets_from_jira(start_date, end_date)
-    cycle_times_per_month = defaultdict(list)
+    cycle_times_per_month = defaultdict(lambda: defaultdict(list))
 
-    
     for index, issue in enumerate(tickets):
         cycle_time, month_key = calculate_cycle_time_seconds(start_date, issue)
         if cycle_time:
-            cycle_times_per_month[month_key].append(cycle_time)
+            team = get_team(issue)
+            cycle_times_per_month[team][month_key].append(cycle_time)
+            verbose_print(f"Processing ticket key issue.key, cycle time: {cycle_time} seconds --> in days: {cycle_time/(seconds_to_hours * hours_to_days):.2f}")
+            cycle_times_per_month["all"][month_key].append(cycle_time)
 
-    #calculate_cycle_time(cycle_times_per_month)
-#     calculate_average_cycle_time_per_month(cycle_times_per_month)
-#     calculate_median_cycle_time_per_month(cycle_times_per_month)
-#     calculate_total_average_cycle_time(cycle_times_per_month)
-#     calculate_total_median_cycle_time(cycle_times_per_month)
-
-
-# def calculate_average_cycle_time_per_month(cycle_times_per_month):
-#     for month, cycle_times in cycle_times_per_month.items():
-#         # ignore if month is not within the current year 
-#         if not month.startswith(str(datetime.now().year)):
-#             continue 
-
-#         print(f"Month: {month}")
-#         if cycle_times:
-#             average_cycle_time = sum(cycle_times) / len(cycle_times)
-#             average_cycle_time_days = average_cycle_time / (60 * 60 * 24)  # Convert seconds to days
-#             print(f"Average Cycle Time: {average_cycle_time_days:.2f}")
-#         else:
-#             print("No completed tickets found.")
-#             print("---")
-
-# def calculate_total_average_cycle_time(cycle_times_per_month):
-#     all_cycle_times = []
-#     for cycle_times in cycle_times_per_month.values():
-#         all_cycle_times.extend(cycle_times)
-
-#     if all_cycle_times:
-#         total_average_cycle_time = sum(all_cycle_times) / len(all_cycle_times)
-#         total_average_cycle_time_days = total_average_cycle_time / (60 * 60 * 24)  # Convert seconds to days
-#         print(f"Total Average Cycle Time: {total_average_cycle_time_days:.2f} days")
-#     else:
-#         print("No completed tickets found to calculate average cycle time.")
+    return cycle_times_per_month
 
 
-# def calculate_median_cycle_time_per_month(cycle_times_per_month):
-#     for month, cycle_times in cycle_times_per_month.items():
-#         # ignore if month is not within the current year 
-#         if not month.startswith(str(datetime.now().year)):
-#             continue 
+def calculate_average_cycle_time(cycle_times):
+    if cycle_times:
+        verbose_print (f"Collected #{len(cycle_times)} cycle times: {cycle_times}")
+        for cycle_time in cycle_times:
+            verbose_print(f"Cycle time: {cycle_time} seconds --> in workhour-days: {cycle_time/(seconds_to_hours * hours_to_days):.2f}")
+        return sum(cycle_times) / len(cycle_times)
+    return 0
 
-#         if cycle_times:
-#             median_cycle_time = statistics.median(cycle_times)
-#             median_cycle_time_days = median_cycle_time / (60 * 60 * 24)  # Convert seconds to days
-            
-#             print(f"Month: {month}")
-#             print(f"Median Cycle Time: {median_cycle_time_days:.2f} days")
-#         else:
-#             print(f"Month: {month}")
-#             print("No completed tickets found.")
-#             print("---")
-            
-# def calculate_total_median_cycle_time(cycle_times_per_month):
-#     all_cycle_times = []
-#     for cycle_times in cycle_times_per_month.values():
-#         all_cycle_times.extend(cycle_times)
+def calculate_median_cycle_time(cycle_times):
+    if cycle_times:
+        return statistics.median(cycle_times)
+    return 0
 
-#     if all_cycle_times:
-#         median_cycle_time = statistics.median(all_cycle_times)
-#         median_cycle_time_days = median_cycle_time / (60 * 60 * 24)  # Convert seconds to days
-#         print(f"Total Median Cycle Time: {median_cycle_time_days:.2f} days")
-#     else:
-#         print("No completed tickets found to calculate median cycle time.")
+def print_cycle_time_metrics(cycle_times_per_month):
+    # Separate the "all" team from other teams -- we want to see this data last. 
+    all_team = cycle_times_per_month.pop("all", None)
+    
+    # Print metrics for all other teams
+    for team, months in sorted(cycle_times_per_month.items()):
+        print(f"Team: {team.capitalize()}")
+        for month, cycle_times in sorted(months.items()):
+            average_cycle_time_s = calculate_average_cycle_time(cycle_times)
+            median_cycle_time_s = calculate_median_cycle_time(cycle_times)
+            average_cycle_time_days = average_cycle_time_s / (seconds_to_hours * hours_to_days)  # business seconds --> hours to business days
+            print(f"Month: {month}, Average Cycle Time: {average_cycle_time_days:.2f} days, Median Cycle Time: {median_cycle_time_s / (seconds_to_hours * hours_to_days):.2f} days")
+    
+    # Print metrics for the "all" team last
+    if all_team:
+        print("Team: All")
+        for month, cycle_times in sorted(all_team.items()):
+            average_cycle_time_s = calculate_average_cycle_time(cycle_times)
+            median_cycle_time_s = calculate_median_cycle_time(cycle_times)
+            average_cycle_time_days = average_cycle_time_s / (seconds_to_hours * hours_to_days)  # business seconds --> hours to business days
+            print(f"Month: {month}, Average Cycle Time: {average_cycle_time_days:.2f} days, Median Cycle Time: {median_cycle_time_s / (seconds_to_hours * hours_to_days):.2f} days")
 
-def business_time_spent_in_seconds(start, end):
-    """extract only the time spent during business hours from a jira time range -- only count 8h"""
-    weekdays = [0, 1, 2, 3, 4]  # Monday to Friday
-    total_business_seconds = 0
-    seconds_in_workday = 8 * 60 * 60  # 8 hours * 60 minutes * 60 seconds
-
-    current = start
-    while current <= end:
-        if current.weekday() in weekdays:
-            day_end = current.replace(hour=23, minute=59)
-            remaining_time_today = day_end - current
-
-            if current.date() != end.date():
-                total_business_seconds += min(remaining_time_today.total_seconds(), seconds_in_workday)
-                current += timedelta(days=1)
-                current = current.replace(hour=0, minute=0)
-            else:
-                remaining_time_on_last_day = end - current
-                total_business_seconds += min(remaining_time_on_last_day.total_seconds(), seconds_in_workday)
-                break
-        else:
-            current += timedelta(days=1)
-            current = current.replace(hour=0, minute=0)
-
-    return total_business_seconds
 
 def main():
     current_year = datetime.now().year
     start_date = f"{current_year}-01-01"
-    end_date = f"{current_year}-12-30"
-    calculate_monthly_cycle_time(start_date, end_date)
+    end_date = f"{current_year}-12-31"
+    cycle_times_per_month = calculate_monthly_cycle_time(start_date, end_date)
+    print_cycle_time_metrics(cycle_times_per_month)
 
 if __name__ == "__main__":
     main()
